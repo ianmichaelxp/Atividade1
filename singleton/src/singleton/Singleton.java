5850e483aa8ea76b75e3097379e79087fc0586db
Ian Michael <37078140+ianmichaelxp@users.noreply.github.com>
2019-09-25 11:42:06 -0300

    atualizacao melhoria singleton

--- singleton/src/singleton/Singleton.java
+++ singleton/src/singleton/Singleton.java
@@ -2,13 +2,13 @@ package singleton;
 
 public class Singleton {
 	
-	private static Singleton instanciaUnica;
+	private volatile static Singleton instanciaUnica;
 	
 	private Singleton() {
 		
 	}
 	
-	public static Singleton getSingleton() {
+	public static synchronized Singleton getSingleton() {
 		if(instanciaUnica == null) {
 			instanciaUnica = new Singleton();
 		}
@@ -17,7 +17,7 @@ public class Singleton {
 	public static void main(String []args) {
 		Singleton a = getSingleton();
 		Singleton b = getSingleton();
-		System.out.println(a + "-" + b);
+		System.out.println("A = " + a + " " +"B = " + b);
 	}
 	
 }
